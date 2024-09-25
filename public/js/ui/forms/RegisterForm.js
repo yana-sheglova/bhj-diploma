86a102cd52f0fdbab5fb54a5baf9204cb97a0c53
Yana Sheglova <yana.shheglova.98@bk.ru>
2024-09-25 18:34:40 +0300

    Point 3.1 done

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -10,6 +10,15 @@ class RegisterForm extends AsyncForm {
    * и закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    User.register(data, (err, response) => {
+      if(err) {
+        console.error(err);
+        alert('Ошибка регистрации! Попробуйте снова.');
+      } else {
+        this.reset();
+        App.setState('user-logged');
+        Modal.close();
+      }
+    });
   }
 }
